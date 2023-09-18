9f09d8c33451fb6338b8900c08d3bc28aa986689
a-maystorov <alkin971@gmail.com>
2023-09-18 20:32:33 +0200

    refactor: simplify query object

--- src/components/PlatformSelector.tsx
+++ src/components/PlatformSelector.tsx
@@ -4,11 +4,17 @@ import usePlatforms, { Platform } from "../hooks/usePlatforms";
 
 interface Props {
   onSelectedPlatform: (platform: Platform) => void;
-  selectedPlatform: Platform | null;
+  selectedPlatformId?: number;
 }
 
-const PlatformSelector = ({ onSelectedPlatform, selectedPlatform }: Props) => {
+const PlatformSelector = ({
+  onSelectedPlatform,
+  selectedPlatformId,
+}: Props) => {
   const { data: platforms, error } = usePlatforms();
+  const selectedPlatform = platforms?.results.find(
+    (p) => p.id === selectedPlatformId
+  );
 
   if (error) return null;
 
